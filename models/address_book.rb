dae61aa825ad9d053cb798e5097eef90b334ba47
kristru <krissafuentes@gmail.com>
2018-04-05 12:14:40 -0600

    implement TDD

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_reader :entries
@@ -16,6 +17,18 @@ class AddressBook
       index += 1
     end
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def import_from_csv(file_name)
+    #starts by reading the file using File.reading
+    csv_text = File.read(file_name)
+    #parses the CSV and returns an object with tables
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    #iterates over each table row, turns it into a hash and then adds it as an entry
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 
   def remove_entry(name, phone_number, email)
