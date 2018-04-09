f0378f51ba30f10aedc4e8de885859f5c933b7cb
kristru <krissafuentes@gmail.com>
2018-04-08 19:13:10 -0600

    implement binary search

--- models/address_book.rb
+++ models/address_book.rb
@@ -29,7 +29,6 @@ class AddressBook
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-    print @entries
   end
 
   def remove_entry(name, phone_number, email)
@@ -41,4 +40,33 @@ class AddressBook
     end
   end
 
-end
+  def binary_search(name)
+    #this saves the leftmost/first item in the array in a variable named lower
+    #and the rightmost/last item in the array as upper
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      #here were finding the middle index, ruby rounds down decimals(ex: 5 - 0, mid = 2)
+      mid = (lower + upper) / 2
+      #storing the middle index value to mid_name
+      mid_name = entries[mid].name
+
+      #comparing the passed in entry to the mid_name
+      if name == mid_name
+        return entries[mid]
+      #if name is alphabetically before mid_name then we set upper to mid - 1
+      #because that means the name must be in the lower half of the array
+      elsif name < mid_name
+        upper = mid - 1
+      #if name is after, the we set lower to mid + 1, since name is in the upper
+      #half of the array
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
+
+    end
+  end
