2101de8a10fc80dc56952a0478fe964b470340f4
kristru <krissafuentes@gmail.com>
2018-04-10 12:26:51 -0600

    add submenu and menu controllers

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -93,7 +93,10 @@ class MenuController
     case selection
     when "n"
     when "d"
+      delete_entry(entry)
     when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
     when "m"
     else
       system "clear"
@@ -120,10 +123,64 @@ class MenuController
     puts "New entry created"
   end
 
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+#use !attribute.empty? to set attribute only if a valid one is given in input
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry:"
+    puts entry
+  end
+
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = address_book.binary_search(name)
+    system "clear"
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
   def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+#here we import the specified file with import_from_csv on address_book
+#the screen will then clear and print the number of entries read from the File
+#begin/rescue wraps protect the program from crashing if an exception is thrown
+    begin
+      entry_count = address_book.import_from_csv(file_name).entry_count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
 
 end
